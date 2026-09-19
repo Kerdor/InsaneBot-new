28102ad51063b7ad758d63a9d83c11aea9bd65b1
Kerdor <59965225+Kerdor@users.noreply.github.com>
2026-09-19 17:07:43 +0300

    Added color and formatter for logging

--- bot/logger.py
+++ bot/logger.py
@@ -1,4 +1,30 @@
 import logging
 
+
+class ColorFormatter(logging.Formatter):
+    COLORS = {
+        logging.DEBUG: "\033[90m",
+        logging.INFO: "\033[37m",
+        logging.WARNING: "\033[33m",
+        logging.ERROR: "\033[31m",
+        logging.CRITICAL: "\033[91m",
+    }
+
+    RESET = "\033[0m"
+
+    def format(self, record):
+        color = self.COLORS.get(record.levelno, self.RESET)
+        message = super().format(record)
+        return f"{color}{message}{self.RESET}"
+
 def setup_logging():
-    logging.basicConfig(level=logging.INFO)
+    handler = logging.StreamHandler()
+
+    formatter = ColorFormatter("[%(levelname)s] %(message)s")
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+
+    logging.getLogger("disnake").setLevel(logging.WARNING)
--- main.py
+++ main.py
@@ -6,13 +6,14 @@ from bot.config import TOKEN
 from bot.extensions import load_cogs
 from bot.logger import setup_logging
 
+setup_logging()
 
 bot = commands.InteractionBot()
-setup_logging()
 
 @bot.event
 async def on_ready():
     logging.info(f"Бот запущен: {bot.user}")
+
 
 load_cogs(bot)
 bot.run(TOKEN)
